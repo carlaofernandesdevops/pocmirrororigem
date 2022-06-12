c7d215f7898a94380cdd6c7c7d0fd99f6f6eb46f
carlaofernandesdevops <107215670+carlaofernandesdevops@users.noreply.github.com>
2022-06-12 01:04:45 -0300

    Add files via upload

---
+++ ConsoleAppPocRepo/ConsoleAppPocRepo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPocRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Espelhamento de Dados");
+            Console.ReadLine();
+        }
+    }
+}
