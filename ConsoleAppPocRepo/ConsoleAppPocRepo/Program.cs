b154d425cdbaea2323d5a284e37be41058af28eb
carlaofernandesdevops <107215670+carlaofernandesdevops@users.noreply.github.com>
2022-06-12 21:48:25 -0300

    Teste erro compilacao 21h48

--- ConsoleAppPocRepo/ConsoleAppPocRepo/Program.cs
+++ ConsoleAppPocRepo/ConsoleAppPocRepo/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppPocRepo
         static void Main(string[] args)
         {
             Console.WriteLine("Teste Espelhamento de Dados");
-            Console.WriteLine("Teste Dados modificados ok");
+            Console.WriteLine("Teste Dados modificados error2148);
             Console.ReadLine();
         }
     }
